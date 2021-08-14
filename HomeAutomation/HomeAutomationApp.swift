d0ec19f56929fca691f99178d6fe9f92003ce172
Gino Sponton <ginosponton@gmail.com>
2021-08-14 17:57:19 +0200

    Initial Commit

---
+++ HomeAutomation/HomeAutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAutomationApp.swift
+//  HomeAutomation
+//
+//  Created by Gino Sponton on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeAutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
